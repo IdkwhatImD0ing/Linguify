43e43d8642d0ecb20072ed59307f05d9d2b2a53f
warren yun <wyun2006@gmail.com>
2024-09-14 11:37:33 -0400

    basic db interfaces

--- client/types/api.ts
+++ client/types/api.ts
@@ -10,3 +10,31 @@ export interface RetellAIResponse {
     id: string;
     status: string;
 }
+
+export interface User {
+  email: string;
+  firstname: string;
+  lastname: string;
+  currentStreak: number;
+  highestStreak: number;
+  uploadedImages: string[];
+  interactions: Interaction[];
+}
+
+export interface Interaction {
+  callId: string;
+  feedback: Feedback;
+}
+
+export interface Feedback {
+  grammarRating: number;
+  grammarSummary: string;
+  fluencyRating: number;
+  fluencySummary: string;
+  vocabularyRating: number;
+  vocabularySummary: string;
+  coherenceRating: number;
+  coherenceSummary: string;
+  engagementRating: number;
+  engagementSummary: string;
+}
